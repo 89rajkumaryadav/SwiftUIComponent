6f3905d7b4abe57385fc78a8700f8b3cb9d526b0
rajkumaryadav89 <rajkumar.yadav@ecolane.com>
2022-08-26 15:12:29 +0530

    Initial Commit

---
+++ SwiftUIComponent/SwiftUIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentApp.swift
+//  SwiftUIComponent
+//
+//  Created by Rajkumar Yadav on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
